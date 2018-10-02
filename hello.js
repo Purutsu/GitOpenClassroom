fc9d23e81143d8b9cfa3bc151bbab2b120b1ba00
Christophe <christbour@modulonet.fr>
2018-10-02 15:11:53 +0200

    ajout d'un prompt dans le fichier javascript qui affichera le texte de votrechoix dans la console

--- hello.js
+++ hello.js
@@ -1,1 +1,3 @@
-console.log("j'ai contaminé vôtre pc, vous êtes maintenant sous mon contrôle mental")
+console.log("j'ai contaminé vôtre pc, vous êtes maintenant sous mon contrôle mental");
+var Hello = prompt("Salut!");
+console.log(Hello);
